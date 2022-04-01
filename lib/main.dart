91ec033afbedfb48c805dc7e0eec1b40abe09bc3
viper <thaparam78666@gmail.com>
2022-04-01 11:05:00 +0545

    row column using widgets

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,35 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("my blog"),
-      ),
-      body: Container(
-        child: Center(child: Text("This is flutter")),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("my blog"),
+        ),
+        body: Container(
+            color: Colors.purple,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(9),
+                  width: 100,
+                  height: 100,
+                  color: Colors.green,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(9),
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(9),
+                  width: 100,
+                  height: 100,
+                  color: Colors.blue,
+                )
+              ],
+            )));
   }
 }
